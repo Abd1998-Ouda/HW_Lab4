3fdc3d60691cbf81b2380e081325eacbafc6ead7
Abd1998-Ouda <63639922+Abd1998-Ouda@users.noreply.github.com>
2020-12-27 19:28:42 +0200

    finish homework

---
+++ README.md
@@ -0,0 +1,7 @@
+# BackEnd homework
+
+## Use
+
+Install the dependencies (Express & Body-parser)
+
+See `img` folder.
--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-not empty
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/greeting/:name', (req, res) => {
+    res.send(JSON.parse(`{ "message": "Hello ${req.params.name}" }`));
+});
+
+app.post('/data', (req, res) => {
+    console.log(req.body);
+    req.body['name'] = 'Abdullah Ouda';
+    res.send(req.body);
+});
+
+app.listen(port);
